c885c16b667377700ccba04cc081f3094fd045fb
phuong090802 <20110547@student.hcmute.edu.vn>
2024-02-17 20:39:26 +0700

    Delete folder mobile

--- TuVanSinhVien/screens/FAQ.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function FAQ() {
-  return (
-    <View>
-      <Text>FAQ</Text>
-    </View>
-  );
-}
--- TuVanSinhVien/screens/Notification.js
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function Notification() {
-  return (
-    <View>
-      <Text>Notification</Text>
-    </View>
-  );
-}
