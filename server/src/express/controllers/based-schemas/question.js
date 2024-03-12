b6af58e655dafc296eddb8c55da1ae5f98969a09
phuong090802 <20110547@student.hcmute.edu.vn>
2024-03-12 23:29:14 +0700

    Update response questions

--- server/src/express/controllers/based-schemas/question.js
+++ server/src/express/controllers/based-schemas/question.js
@@ -10,7 +10,6 @@ export const questionsHandler = catchAsyncErrors(async (req, res, next) => {
     status: 'publicly-answered-and-approved',
     // status: 'publicly-answered-pending-approval', // for test
   })
-    .lean()
     .populate({
       path: 'answer',
       populate: {
@@ -22,13 +21,14 @@ export const questionsHandler = catchAsyncErrors(async (req, res, next) => {
       path: 'user',
       select: '_id fullName avatar',
     })
+    .lean()
     .select('_id title content createdAt views user answer');
   const queryAPI = new QueryAPI(query, req.query).search().filter().sort();
   let questionRecords = await queryAPI.query;
   const numberOfQuestions = questionRecords.length;
   questionRecords = await queryAPI.pagination().query.clone();
   const {
-    data: questions,
+    data: retQuestions,
     page,
     pages,
   } = paginateResults(
@@ -37,6 +37,34 @@ export const questionsHandler = catchAsyncErrors(async (req, res, next) => {
     req.query.size,
     questionRecords
   );
+
+  const questions = retQuestions.map((question) => {
+    const user = {
+      ...question.user,
+      avatar: question.user.avatar.url,
+    };
+
+    const counsellor = {
+      _id: question.answer.user._id,
+      fullName: question.answer.user.fullName,
+      avatar: question.answer.user.avatar.url,
+    };
+
+    const answer = {
+      ...question.answer,
+      user: counsellor,
+      fileURL: question.answer.file.url,
+    };
+
+    delete answer.file;
+
+    return {
+      ...question,
+      user,
+      answer,
+    };
+  });
+
   res.json({
     success: true,
     questions,
